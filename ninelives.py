872cad1eef72d2c5c8038b4a5f52267855b59ba3
Sudheer <pokalasudheer143@gmail.com>
2024-01-31 23:35:27 -0500

    lives

---
+++ ninelives.py
@@ -0,0 +1,6 @@
+import random
+lives = 9
+words = ['Sudheer','Reddy',"python", "jumble", "easy", "difficult", "answer",  "xylophone"]
+secret_word = random.choice(words)
+clue = list('?????')
+heart_symbol
